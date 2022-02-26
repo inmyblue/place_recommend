c55ff310065b41f4a9dde67847637258606d883a
inmyblue <inmyblue0930@naver.com>
2022-02-26 17:03:13 +0900

    Basic Setting

--- app.py
+++ app.py
@@ -13,5 +13,5 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-if __name__ == '__main__':`
+if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)

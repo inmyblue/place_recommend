3642b261980c55d90c784abfeca76bd15cfb8e4f
inmyblue <inmyblue0930@naver.com>
2022-02-26 16:20:02 +0900

    basic setting

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from pymongo import MongoClient
+import certifi
+
+#DB Configure
+client = MongoClient('mongodb+srv://pre_project:soaktth11@cluster0.qgqev.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=certifi.where())
+db = client.recommend_place
+
+#Flask App Setup
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)

ad418fcf24797f40949476e14a4556469d9c2c4c
insidelamp <93029270+insidelamp@users.noreply.github.com>
2022-02-27 22:03:49 +0900

    db 주소 수정 및 자잘한 사항 추가

--- sy/register-page.py
+++ sy/register-page.py
@@ -1,17 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
 from pymongo import MongoClient
 import certifi
 
-#DB Configure
-client = MongoClient('mongodb+srv://pre_project:soaktth11@cluster0.qgqev.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=certifi.where())
-db = client.recommend_place
+client = MongoClient('mongodb+srv://text:sparta@cluster0.qerty.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=certifi.where())
+db = client.dbsparta
 
-#Flask App Setup
-app = Flask(__name__)
+doc = {
+    'name':'bob',
+    'age':27
+}
+db.sy.insert_one(doc)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('register-page.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)

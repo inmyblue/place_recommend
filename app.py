b1afb8f62ba8918c5d3e0d59f6e6bff678bd6b03
insidelamp <rkdhfh@naver.com>
2022-02-28 22:52:00 +0900

    불필요한 파일 정리

--- app.py
+++ app.py
@@ -1,17 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
 from pymongo import MongoClient
 import certifi
 
-#DB Configure
-client = MongoClient('mongodb+srv://pre_project:soaktth11@cluster0.qgqev.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=certifi.where())
-db = client.recommend_place
+import requests
+from bs4 import BeautifulSoup
 
-#Flask App Setup
-app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+   return render_template('index.html')
+
+@app.route("/food", methods=["POST"])
+def food_post():
+    name_receive = request.form["name_give"]
+    title_receive = request.form["title_give"]
+    comment_receive = request.form["comment_give"]
+    suggestion_receive = request.form["suggestion_give"]
+    url = request.form["shop_give"]
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url, headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+    #db 주소를 넣어주세요!
+    client = MongoClient('여기에~',tlsCAFile=certifi.where())
+    db = client.dbsparta
+
+    foods = soup.select('body')
+
+    for food in foods:
+        shop = soup.select_one('meta[property="og:title"]')['content']
+        address = food.select_one('main > article > div.column-wrapper > div.column-contents > div > section.restaurant-detail > table > tbody > tr:nth-child(1) > td').text
+        image = soup.select_one('meta[property="og:image"]')['content']
+        menu = soup.select_one('main > article > div.column-wrapper > div.column-contents > div > section.restaurant-detail > table > tbody > tr:nth-child(3)').text
+
+    doc = {
+        'name': name_receive,
+        'title': title_receive,
+        'shop': shop,
+        'menu': menu,
+        'comment': comment_receive,
+        'suggestion': suggestion_receive,
+        'image': image,
+        'address':address,
+
+    }
+
+    db.foods.insert_one(doc)
+    return jsonify({'msg':'맛집 등록 완료!'})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+   app.run('0.0.0.0', port=5000, debug=True)

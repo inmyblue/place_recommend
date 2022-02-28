40da898c53f9732066d87b39988dcba034c51eff
inmyblue <inmyblue0930@naver.com>
2022-02-28 22:38:48 +0900

    2022_02_28 로그인/환경설정변수

---
+++ login.py
@@ -0,0 +1,48 @@
+from flask import Flask, Blueprint, render_template, jsonify, request
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+
+import certifi
+import os
+import hashlib
+import jwt
+import bcrypt
+
+
+#환경변수 값 불러오기
+load_dotenv()
+
+#DB Configure
+mongo_host = os.getenv('MONGODB_HOST')
+client = MongoClient(mongo_host, tlsCAFile=certifi.where())
+db = client.recommend_place
+
+login_bp = Blueprint('login', __name__)
+app = Flask(__name__)
+
+@login_bp.route('/')
+def login():
+    return render_template('login.html')
+
+@login_bp.route('/register', methods=['POST'])
+def register():
+    reg_id = request.form['reg_id']
+    reg_pwd = request.form['reg_pwd']
+    reg_name = request.form['reg_name']
+
+    sha_pwd = hashlib.sha256(reg_pwd.encode()).hexdigest()
+
+    id_chk = db.member.find_one({'member_id' : reg_id}, {'_id' : False})
+    if id_chk :
+        return jsonify({"msg" : "이미 가입된 ID입니다"})
+
+    doc = {
+        'member_id' : reg_id,
+        'member_pw' : sha_pwd,
+        'member_name' : reg_name
+    }
+
+    db.member.insert_one(doc)
+
+    return jsonify({"msg" : "회원가입이 완료되었습니다"})
